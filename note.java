9668d5b38860118b828c9b9c0cf55e157a826448
olatoye153 <paulenny1998@gmail.com>
2023-01-26 14:06:26 +0100

    First commit

---
+++ note.java
@@ -0,0 +1,18 @@
+import javax.swing.*;  
+import java.awt.event.*;  
+class PopupMenuExample {  
+     PopupMenuExample(){  
+         final JFrame f= new JFrame("PopupMenu Example");  
+         final JPopupMenu popupmenu = new JPopupMenu("Edit");   
+         JMenuItem cut = new JMenuItem("Cut");  
+         JMenuItem copy = new JMenuItem("Copy");  
+         JMenuItem paste = new JMenuItem("Paste");  
+         popupmenu.add(cut); popupmenu.add(copy); popupmenu.add(paste);        
+         f.addMouseListener(new MouseAdapter() {  
+            public void mouseClicked(MouseEvent e) {              
+                popupmenu.show(f , e.getX(), e.getY());  
+            }                 
+         });  
+         f.add(popupmenu);   
+    
+}
